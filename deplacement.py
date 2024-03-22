c0ef2d0b4ee76058ce4facc0126d6a05a11e66d2
0xb0c4n <78223266+Womercode@users.noreply.github.com>
2024-03-22 10:21:24 +0000

    Deplacement
    Deplacement X

---
+++ deplacement.py
@@ -0,0 +1,7 @@
+def deplacement_x(perso_x, speed):
+  """Prend en compte le x du personnage (integer) et la vitesse (float)
+  Renvoie le nouveau x (integer)"""
+  perso_x += 1 * speed
+  return perso_x
+
+def
--- main.py
+++
@@ -1,3 +0,0 @@
-def deplacement_x(perso_x):
-  """Prend en compte le x du personnage
-  Renvoie le """
