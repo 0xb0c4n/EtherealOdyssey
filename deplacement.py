028aa289456dfb4a0aca0efa38b5b9149bfe82fb
Alexandre <womercode@gmail.com>
2024-03-31 22:38:12 +0200

    déplacement vers la gauche + animation du sprite

--- deplacement.py
+++ deplacement.py
@@ -1,7 +1,20 @@
+import pyxel
+
 def deplacement_x(perso_x, speed):
   """Prend en compte le x du personnage (integer) et la vitesse (float)
   Renvoie le nouveau x (integer)"""
-  return perso_x
+  if(pyxel.btn(pyxel.KEY_RIGHT)):
+    perso_x+=3*speed
+    animation="run"
+    direction=1
+  elif(pyxel.btn(pyxel.KEY_LEFT)):
+    animation="run"
+    direction=-1
+    perso_x+=3*speed
+  else:
+    animation="idle"
+    direction=1
+  return perso_x, animation, direction
 
 def jump(perso_y, height):
   """Prend en compte le y du personnage (integer) et height(float)
--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import pyxel
-from deplacement import *
-
-pyxel.init(160, 120)
-
+from deplacement import deplacement_x
+run_sprite = [(48,0,38,58),(88,0,29,56),(120,0,32,56),(152,0,33,50),(192,0,43,51),(0,64,31,51)]
+pyxel.init(500, 400, "Ethereal Odyssey", display_scale=2)
+pyxel.load("ressources.pyxres")
+perso_x = 0
+animation="idle"
 def update():
-    if pyxel.btnp(pyxel.KEY_Q):
-        pyxel.quit()
+    global perso_x, animation, direction
+
+    perso_x, animation, direction = deplacement_x(perso_x, 1)
 
 def draw():
-  pyxel.rect(50, 0, 8, 8, 9)
-  
+    pyxel.cls(0)
+    if(animation == "run"):
+        coef = pyxel.frame_count // 5 % 5
+        pyxel.blt(perso_x,150,0,run_sprite[coef][0],run_sprite[coef][1],run_sprite[coef][2],run_sprite[coef][3]) 
+    else:
+        pyxel.blt(perso_x,150,0,0,0,24,58) 
+
 pyxel.run(update, draw)
