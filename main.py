023a7ae6ee39237fc193211a5f06a8d894d572b1
Alexandre <womercode@gmail.com>
2024-04-07 12:32:22 +0200

    Ajout du sorcier + interaction. Système d'affichage de la quête en cours

--- main.py
+++ main.py
@@ -9,33 +9,41 @@ jump_sprite = [(0, 120, 28, 53), (32, 120, 36, 56), (72, 120, 31, 50),
                (104, 120, 32, 60), (136, 120, 38, 53), (176, 120, 25, 46)]
 fireball_sprite = [(0, 184, 32, 56), (32, 184, 39, 56), (72, 184, 48, 53),
                    (120, 184, 50, 53), (168, 184, 7, 8)]
+pal = [0xffffff,0x8c938c,0x5a3936,0x28222c,0x4c505b,0x73522d,
+       0x83604f,0x3c4c54,0xc49892,0x3c445c,0x6c6e6c,0x7c706a,
+       0x6c6468,0xf3b340,0xe68d02,0xffb228,0xAF082D,0x83213C,
+       0xF35C5C,0x2D2E4D,0x3A072B]
+
 pyxel.init(500, 400, "Ethereal Odyssey", display_scale=2)
 pyxel.load("ressources.pyxres")
-
+pyxel.colors.from_list(pal)
 perso_x = 0
 y = 150
 scroll_x = 0
 SCROLL_BORDER_X = 225
 animation = "fireball"
 pos_x_wizard = 400
-pos_y_wizard = 41
+pos_y_wizard = 140
 is_jumping = False
 is_descending = False
 is_inside = False
-questNumber = get_player()['questNumber']
-main_questNb = int(questNumber)
-print(get_quests())
-
+launch = False
+title = ""
 
 def update():
-  global perso_x, animation, direction, y, scroll_x, is_jumping, is_descending, is_inside
-  if perso_x < scroll_x:
-    perso_x = scroll_x
-  elif perso_x > scroll_x + SCROLL_BORDER_X:
-    scroll_x = min(perso_x - SCROLL_BORDER_X, 240 * 8)
+  global perso_x, animation, direction, y, scroll_x, is_jumping, is_descending, is_inside, title, launch
+  if perso_x > scroll_x + SCROLL_BORDER_X and direction == 1:
+    scroll_x = perso_x - SCROLL_BORDER_X
+  elif perso_x > scroll_x + SCROLL_BORDER_X and direction == -1:
+    scroll_x = perso_x
+
 
   perso_x, animation, direction = deplacement_x(perso_x, 1)
   is_inside = start_quest(perso_x, pos_x_wizard)
+  questNumber = get_player()['questNumber']
+  if(is_inside == True):
+    launch, title = launch_quest(questNumber, get_quests(), launch, title)
+
   if pyxel.btnr(pyxel.KEY_Z):
     animation = "fireball"
 
@@ -58,15 +66,19 @@ def draw():
   pyxel.cls(0)
   pyxel.camera(scroll_x, 0)
   pyxel.images[1].load(0, 0, "assets/wizard.png")
-  pyxel.blt(pos_x_wizard, pos_y_wizard, 1, 0, 0, 153, 191)
+  pyxel.images[2].load(0, 0, "assets/heart.png")
+  pyxel.blt(pos_x_wizard, pos_y_wizard, 1, 0, 0, 153, 191, 12)
+  pyxel.blt(scroll_x, 0, 2, 0, 0, 153, 23, 23)
 
   #Démarrage de la quête
 
   if (is_inside == True):
-    pyxel.text(scroll_x, 0, "Press [E] to interact", 12)
+    pyxel.text(375, 130, "Press [E] to interact", 12)
+    if(launch):
+      pyxel.text(500, 0, title, 12)
 
   #Animation
-  if (animation == "run"):
+  if (animation == "run" and is_jumping == False):
     coef = pyxel.frame_count // 5 % 5
     pyxel.blt(perso_x, y, 0, run_sprite[coef][0], run_sprite[coef][1],
               run_sprite[coef][2] * direction, run_sprite[coef][3], 0)
@@ -75,14 +87,13 @@ def draw():
     pyxel.blt(perso_x, y, 0, fireball_sprite[coef][0],
               fireball_sprite[coef][1], fireball_sprite[coef][2],
               fireball_sprite[coef][3], fireball_sprite[coef][4], 0)
-  elif (y != 150):
+  elif (y != 150 and is_jumping == True):
     coef = pyxel.frame_count // 6 % 6
     print(coef)
     pyxel.blt(perso_x, y, 0, jump_sprite[coef][0], jump_sprite[coef][1],
               jump_sprite[coef][2] * direction, jump_sprite[coef][3], 0)
   else:
-    coef = 0
-    pyxel.blt(perso_x, y, 0, 0, 0, 24, 58)
+    pyxel.blt(perso_x, y, 0, 0, 0, 24*direction, 58, 0)
 
 
 pyxel.run(update, draw)
--- quests.py
+++ quests.py
@@ -1,8 +1,10 @@
+import pyxel
+
 def calc_hitbox(pos_x):
   """Renvoie la hitbox d'un objet (string)
   Prend en paramètre les coordonnées x  de l'objet (integer)"""
-  debut_hitbox = pos_x - 10
-  fin_hitbox = pos_x + 10
+  debut_hitbox = pos_x - 50
+  fin_hitbox = pos_x + 50
   hitbox = str(debut_hitbox) + ";" + str(fin_hitbox)
   return hitbox
 
@@ -20,7 +22,12 @@ def start_quest(perso_x, pos_x_wizard):
   wizard_hitbox = calc_hitbox(pos_x_wizard)
   return is_in_hitbox(perso_x, wizard_hitbox)
 
-def launch_quest(questNumber, finished):
+def launch_quest(questNumber, questDict, launch, title):
   """Vérifie si la quête précédente est terminée, et si oui démarre la nouvelle quête
   Prend en compte le questNumber du joueur, et l'état de la quête précédente"""
-  
+  if(pyxel.btnr(pyxel.KEY_E)):
+    launch = True
+    title = questDict[int(questNumber)]["title"]
+    dialog = questDict[int(questNumber)]["deploy"]["dialog"]
+    
+  return launch, title
