8a0e09c76ebcf9fb3765c0ada24d06dbf7c20ada
Alexandre <womercode@gmail.com>
2024-04-14 12:35:29 +0200

    ajout du panneau d'affichage stonks

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ fireball_sprite = [(0, 184, 32, 56), (32, 184, 39, 56), (72, 184, 48, 53),
 pal = [0x1b2954,0x8c938c,0x5a3936,0x28222c,0x4c505b,0x73522d,
        0x83604f,0x3c4c54,0xc49892,0x3c445c,0x6c6e6c,0x7c706a,
        0x6c6468,0xf3b340,0xe68d02,0xffb228,0xAF082D,0x83213C,
-       0xF35C5C,0x2D2E4D,0x316595,0xffffff]
+       0xF35C5C,0x2D2E4D,0x316595,0xffffff,0x3fb34e,0x000000]
 
 pyxel.init(500, 250, "Ethereal Odyssey", display_scale=2)
 pyxel.load("ressources.pyxres")
 pyxel.colors.from_list(pal)
 
 perso_x = 0
-y = 169
+y = 165
 scroll_x = 0
 SCROLL_BORDER_X = 125
 animation = "fireball"
@@ -49,8 +49,11 @@ def update():
 
   if perso_x > scroll_x + SCROLL_BORDER_X and direction == 1:
     scroll_x += 3
+  elif perso_x < SCROLL_BORDER_X:
+    scroll_x = 0
   elif direction == -1:
     scroll_x -= 3
+
   
   if(is_inside == True):
     if(i == len(get_quests()[0]["deploy"][0]["dialog"])):
@@ -76,7 +79,7 @@ def update():
 
   if (y >= 149 and is_jumping == True):
     y -= 2
-  elif (is_descending == True and y <= 169):
+  elif (is_descending == True and y <= 165):
     y += 2
   elif (y <= 149):
     is_descending = True
@@ -95,7 +98,7 @@ def draw():
     pyxel.camera(scroll_x, 0)
     pyxel.images[2].load(0, 0, "assets/assets1.png")
     pyxel.images[1].load(0, 0, "assets/background.png")
-    for i in range(50):
+    for i in range(10):
       pyxel.blt(256*i, 0, 1, 0,0,256,177)
 
     for elt in pnj_list:
@@ -104,18 +107,20 @@ def draw():
     #Démarrage de la quête
     
     for i in range(1400):
-      pyxel.blt(42*i,250-27,2,1,74,42,27)
+      pyxel.blt(41*i,250-21,2,1,74,41,21)
+    
+    pyxel.blt(200, 0, 2, 47,0,128,100,21)
 
     if (is_inside == True):
-      pyxel.text(375, 130, "Press [E] to interact", 27)
+      pyxel.text(375, 130, "Press [E] to interact", 21)
     
     if(launch):
-      pyxel.text(scroll_x + 160, 5, title, 27)
-      pyxel.text(scroll_x + 150, 15, instruction, 27)
+      pyxel.text(scroll_x + 160, 5, title, 21)
+      pyxel.text(scroll_x + 150, 15, instruction, 21)
 
     if character != "" and dialog != {} and dialog != "":  
-      pyxel.text(scroll_x + 20,135,character.split("_")[0], 27)
-      pyxel.text(scroll_x + 20,150,dialog[character],27)
+      pyxel.text(scroll_x + 20,135,character.split("_")[0], 21)
+      pyxel.text(scroll_x + 20,150,dialog[character],21)
     #Animation
     if (animation == "run" and is_jumping == False):
       coef = pyxel.frame_count // 5 % 5
